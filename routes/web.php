45d3572c51af017d81561cc2b7799b62e3cdb3aa
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2025-03-24 15:10:57 -0300

    Ajustes nos logins

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/sobre', function (){
 
 Route::middleware('guest')->group(function (){
     Route::get('/login', [LoginController::class, 'showLoginPage'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:5,1');
 });
 
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
