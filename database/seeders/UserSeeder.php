a56947c50ec25d39097f8f64d10803b96ca45012
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2026-05-03 14:52:09 -0300

    feat: db user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'adminTeste',
+            'email' => 'admin@serverhub.com',
+            'password' => Hash::make('admin@admin'),
+            'is_admin' => true,
+        ]);
+
+        User::create([
+            'name' => 'test1',
+            'email' => 'test1@serverhub.com',
+            'password' => Hash::make('test1'),
+            'is_admin' => false,
+        ]);
+    }
+}
