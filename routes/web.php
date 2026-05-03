b873298b0f4c5e7277a71895fce18d42ff249a3a
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2026-05-03 17:50:43 -0300

    feat: login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('guest')->group(function (){
+    Route::get('/login', [LoginController::class, 'LoginView'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->middleware('throttle:5,1');
+});
+
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth')->name('logout');
