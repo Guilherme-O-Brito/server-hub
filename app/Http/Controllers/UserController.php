4b529473917fd8332b0ebfa68733273e72bdce8b
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2026-05-11 08:56:37 -0300

    feat: update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    //admin only    
     public function create(Request $request)
     {
         $validated = $request->validate([
@@ -32,4 +34,34 @@ class UserController extends Controller
         return response()->json(['message' => 'User created successfully'], 201);
 
     }
+
+    // admin only
+     public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($id)],
+            'password' => ['nullable', Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()],
+            'is_admin' => ['required', 'boolean'] 
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        if ($validated['password'] != null) {
+            $user->password = Hash::make($validated['password']);
+        }
+        $user->is_admin = $validated['is_admin'];
+
+        $user->save();
+
+        return response()->json(['message' => 'Alteração registrada com sucesso!']);
+
+    }
+
 }
