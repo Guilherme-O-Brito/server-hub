140a551f809d92975d73658cd557f824d0285d0b
Guilherme-O-Brito <guilhermebrito@gec.inatel.br>
2026-08-10 20:36:07 -0300

    fix: favicon

--- resources/views/layouts/top_app_bar.blade.php
+++ resources/views/layouts/top_app_bar.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('imgs/logo/Logo server-hub2.svg') }}">
     @routes
     @auth
         <div id="authUser" data-auth-user=@json([
