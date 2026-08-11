6fe9a6b8d5a1a0460bb406e2d64d890c5e71de3c
Guilherme-O-Brito <guilhermebrito@gec.inatel.br>
2026-08-11 08:08:56 -0300

    chore: removed legacy meta tag

--- resources/views/layouts/top_app_bar.blade.php
+++ resources/views/layouts/top_app_bar.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/svg+xml" href="{{ asset('imgs/logo/Logo server-hub2.svg') }}">
     @routes
